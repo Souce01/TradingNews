071f5f92b0e37d9a398fa75a9c64f3c9f122502a
alexandre soucy <alexandresoucy7@gmail.com>
2020-08-20 15:46:46 -0400

    work in progress

--- TradingNews/static/Site/script.js
+++ TradingNews/static/Site/script.js
@@ -7,5 +7,5 @@ $(".article")
   .hover(function (){
     $(this).find(".article-title").css("color", "#4193c6")
   }, function(){
-      $(this).find(".article-title").css("color", "white")
+    $(this).find(".article-title").css("color", "white")
   })
