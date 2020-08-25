2e6b4dc7bf4b42fd7423649d5c50b3292ed881b5
alexandre soucy <alexandresoucy7@gmail.com>
2020-08-25 19:10:30 -0400

    work in progress

--- TradingNews/static/Site/script.js
+++ TradingNews/static/Site/script.js
@@ -1,12 +1,22 @@
+// removes images that didn't load
 $(".article-img")
-  .on('error', function() {
-    $(this).remove()
+  .on('error', function(e) {
+    $(this).remove();
+    e.preventDefault();
   })
 
+// Changes the color an article's title on hover
 $(".article")
-  .hover(function (){
-    $(this).find(".article-title").css("color", "#4193c6")
-  }, function(){
-    $(this).find(".article-title").css("color", "white")
+  .hover(function (e){
+    $(this).find(".article-title").css("color", "#4193c6");
+    e.preventDefault();
+  }, function(e){
+    $(this).find(".article-title").css("color", "white");
+    e.preventDefault();
   })
 
+// makes an api call to get search endpoint
+$("#search-field").change(function (e) { 
+
+  e.preventDefault();
+});
